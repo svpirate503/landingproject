8283fb4a0a140642a5f44cf078812f7ee89bcf6d
Gabriel Hernández <73655786+svpirate503@users.noreply.github.com>
2024-08-08 16:07:44 -0400

    first commit

---
+++ mylandingpage/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mylandingpage project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylandingpage.settings')
+
+application = get_wsgi_application()
